a7086fac70780f1dbd9eb8bbdc89525c251e37c7
Christian F.W. Schmidt <cfwschmidt@gmail.com>
2022-09-18 02:25:34 -0700

    Update Graph.tsx and DataManipulator.ts to graph ratio, U & L bounds, and trigger alert. Add video walkthrough.

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,37 @@
 import { ServerRespond } from './DataStreamer';
 
+// the interface defining what a Row object will look like
+// this interface is used by the generateRow static function of the DataManipulator defined below.
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    // serverRespond  includes an element for both the ABC and DEF stocks. 
+    // To determine the ratios and alert triggers we perform calculations using both of these elements
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.025;
+    const lowerBound = 1 - 0.025;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      // Use whichever timestamp (ABC or DEF quote) is earliest
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      // If the ratio is above or below the bounds, the trigger alert equals the ratio, other times it's just undefined.
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ?  ratio : undefined,
+    }
   }
 }
